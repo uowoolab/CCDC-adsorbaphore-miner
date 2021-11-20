6abfa7e804d202fbb4306d0384e01239d844e47e
Peter Boyd <peter.g.boyd@gmail.com>
2021-11-20 15:07:32 -0500

    close to the correction

--- search_local_files.py
+++ search_local_files.py
@@ -18,45 +18,70 @@ import os, sys
 import csv
 import numpy as np
 
+# structure stuff
+sys.path.insert(0, '/home/pboyd/modules/fa3ps')
+from faps import Structure, Atom, Cell, minimum_image
+
 avo = 6.02214076e23 
 cm3 = 1e24  # A^3 / cm^3
-
 outdir = os.getcwd()
 struct_dir = sys.argv[1] 
 csd_subsetdir = join(os.environ['CSDHOME'], 'subsets')
 datafile = 'substructure_data.csv'
 
-searcher = ccdc.search.SubstructureSearch()
-benz_smarts = 'a1aaaaa1'
-arom1 = ccdc.search.SMARTSSubstructure(benz_smarts)
-arom2 = ccdc.search.SMARTSSubstructure(benz_smarts)
+def benz_search():
+    searcher = ccdc.search.SubstructureSearch()
+    benz_smarts = 'a1aaaaa1'
+    arom = ccdc.search.SMARTSSubstructure(benz_smarts)
+    sub1 = searcher.add_substructure(arom)
+    # dummy angle and dist constraints so the code below doesn't break
+    searcher.add_angle_constraint('ANGLE', (sub1, 0), (sub1, 1), (sub1, 2), (-360, 360))
+    searcher.add_distance_constraint('DIST', (sub1, 0), (sub1, 1), ('<', 8.0), type='any')
+    
+    return searcher
 
-sub1 = searcher.add_substructure(arom1)
-sub2 = searcher.add_substructure(arom2)
 
-searcher.add_plane('PLANE1', (sub1, 0), (sub1, 1), (sub1, 2),
-                             (sub1, 3), (sub1, 4), (sub1, 5))
+def adsorbaphore_search():
+    searcher = ccdc.search.SubstructureSearch()
+    benz_smarts = 'a1aaaaa1'
+    arom1 = ccdc.search.SMARTSSubstructure(benz_smarts)
+    arom2 = ccdc.search.SMARTSSubstructure(benz_smarts)
+    
+    sub1 = searcher.add_substructure(arom1)
+    sub2 = searcher.add_substructure(arom2)
+    
+    
+    searcher.add_plane('PLANE1', (sub1, 0), (sub1, 1), (sub1, 2),
+                                 (sub1, 3), (sub1, 4), (sub1, 5))
+    
+    searcher.add_plane('PLANE2', (sub2, 0), (sub2, 1), (sub2, 2),
+                                 (sub2, 3), (sub2, 4), (sub2, 5))
+    
+    searcher.add_centroid('CENT1', (sub1, 0), (sub1, 1), (sub1, 2),
+                                   (sub1, 3), (sub1, 4), (sub1, 5))
+    
+    searcher.add_centroid('CENT2', (sub2, 0), (sub2, 1), (sub2, 2),
+                                   (sub2, 3), (sub2, 4), (sub2, 5))
+    
+    # centroid between aromatic planes?
+    searcher.add_centroid('CENT3', 'CENT1', 'CENT2')
+    searcher.add_dummy_point('CENT3D', 0.0, 'CENT3', 'CENT2')
+    
+    # Make sure the angle is between -5 and +5 deg?
+    searcher.add_plane_angle_constraint('ANGLE', 'PLANE1', 'PLANE2', ('<=', 10))
+    searcher.add_distance_constraint('DIST', 'CENT1', 'CENT2', (6.8, 8.4), vdw_corrected=False, type='any')
+    
+    # Make sure the two aromatic planes are aligned
+    searcher.add_vector('VEC1', 'CENT1', (sub1, 0))
+    searcher.add_vector('VECN', 'CENT1', 'CENT2')
+    searcher.add_vector_angle_constraint('ANGLE_N', 'VEC1', 'VECN', (70, 100)) 
 
-searcher.add_plane('PLANE2', (sub2, 0), (sub2, 1), (sub2, 2),
-                             (sub2, 3), (sub2, 4), (sub2, 5))
+    # To check if atoms between adsorbaphore
+    # searcher.add
+    return searcher
 
-searcher.add_centroid('CENT1', (sub1, 0), (sub1, 1), (sub1, 2),
-                               (sub1, 3), (sub1, 4), (sub1, 5))
-
-searcher.add_centroid('CENT2', (sub2, 0), (sub2, 1), (sub2, 2),
-                               (sub2, 3), (sub2, 4), (sub2, 5))
-
-# centroid between aromatic planes?
-searcher.add_centroid('CENT3', 'CENT1', 'CENT2')
-
-# make sure the angle is between -5 and +5 deg?
-searcher.add_plane_angle_constraint('ANGLE', 'PLANE1', 'PLANE2', (0, 10))
-searcher.add_distance_constraint('DIST', 'CENT1', 'CENT2', (3.1, 4.0), vdw_corrected=True, type='any')
-
-# Make sure the two aromatic planes are aligned
-#searcher.add_vector('VEC1', 'CENT1', (sub1, 0))
-#searcher.add_vector('VECN', 'CENT1', 'CENT2')
-#searcher.add_vector_angle_constraint('ANGLE_N', 'VEC1', 'VECN', (80, 100)) 
+searcher = adsorbaphore_search()
+#searcher = benz_search()
 
 # find hits
 start_time = time.time()
@@ -65,8 +90,7 @@ start_time = time.time()
 f = open(join(outdir, datafile), 'w')
 
 cwriter = csv.writer(f)
-cwriter.writerow(['CSD_NAME', 'UNIT_VOL_A^3', 'CRYSTAL_MOLAR_DENS_MMOL_CM^3', 'SUBSTRUCT_COUNT', 'SUBSTRUCT_DENS_MMOL_CM^3', 
-                  'PLANAR_ANGLE', 'PLANAR_ANGLE_STDEV', 'PLANAR_DIST', 'PLANAR_DIST_STDEV'])
+cwriter.writerow(['CSD_NAME', 'UNIT_VOL_A^3', 'CRYSTAL_MOLAR_DENS_MMOL_CM^3', 'SUBSTRUCT_COUNT', 'SUBSTRUCT_DENS_MMOL_CM^3', 'PLANAR_ANGLE', 'PLANAR_ANGLE_STDEV', 'PLANAR_DIST', 'PLANAR_DIST_STDEV'])
 #searcher.settings.no_disorder = 'all'
 #searcher.settings.max_r_factor = 5.0
 # mine just the mofs
@@ -101,18 +125,31 @@ files = [i for i in os.listdir(struct_dir) if i.endswith('.cif')]
 #         'YOZJEE01_clean.cif'
 #         ]
 
-reader = CrystalReader([join(struct_dir, j) for j in files])
-[i.assign_bonds() for i in reader]
+#files = ['str_m4_o29_o29_acs_sym.46.cif']
 
+reader = CrystalReader([join(struct_dir, j) for j in files])
 start_time = time.time()
 # just get unique hits, compute number of hits per structure in the for loop below.
-hits = searcher.search(reader, max_hits_per_structure=1)
-end_time = time.time()
-print('{0:d} structures found in {1:.2f} seconds.'.format(len(hits), elapsed))
+#hits = searcher.search(reader, max_hits_per_structure=1)
+#tot = len(hits)
+tot = len(reader)
 
-tot = len(hits)
-for h in hits:
+#for h in hits:
+for h in reader:
     h.assign_bonds()
+    # create a faps structure
+    fstr = Structure(h.identifier)
+    for atom in h.molecule.atoms:
+        coords = (atom.coordinates.x, atom.coordinates.y, atom.coordinates.z)
+        fstr.atoms.append(Atom(idx=atom.index ,
+                               pos=coords, 
+                               at_type=atom.atomic_symbol, 
+                               mass=atom.atomic_weight))
+        assert (atom.index == len(fstr.atoms)-1)
+
+    pp = (h.cell_lengths.a, h.cell_lengths.b, h.cell_lengths.c,
+            h.cell_angles.alpha, h.cell_angles.beta, h.cell_angles.gamma)
+    fstr.cell.params = pp
     local_hits = searcher.search(h, max_hits_per_structure=10000)
     nhits = len(local_hits)
     print('{0:s} hits: {1:d}'.format(h.identifier, nhits))
@@ -120,11 +157,40 @@ for h in hits:
         rm = []
 
         # make sure nothing is in between the matched substructures
+        # this does not work!
         for i, hit in enumerate(local_hits):
-            benz1, benz2 = hit.match_substructures()
+            #benz1, benz2 = hit.match_substructures()
             atom_list = hit.match_atoms()
+            indices = [i.index for i in atom_list]
+            # COM make sure periodic image shifted. for each aromatic ring
+            a1 = fstr.atoms[indices[0]]
+            
+            shifted_wrt_a1 = minimum_image(a1, [], fstr.cell.cell)
+
+            # make sure middle of COMs is shifted.
+
+            # measure minimum image distance of all non-adsorbaphore atoms
+            # make sure not in van der waals radii.
+
+
+            other_atoms = [i for i in h.molecule.atoms if i.index not in indices]
+            print(other_atoms) 
+            print(hit.dummy_point_objects('CENT3D'))
+            # i.fractional_coordinates, i.vdw_radius, i.coordinates
+
+            sys.exit()
+            vector_object = hit.vector_objects('VECN')
+            print(vector_object)
             # evaluate if an atoms from the first molecule are in line of sight with the second
-            eval_ = [a.is_in_line_of_sight(b) for a in atom_list[:6] for b in atom_list[6:]]
+            #eval_ = [a.is_in_line_of_sight(b) for a in atom_list[:6] for b in atom_list[6:]]
+            eval_ = [a.is_in_line_of_sight(b) for (a,b) in zip(atom_list[:6], atom_list[6:])]
+            # vector and find all atoms closest points to vector.
+            # Benchmark this.
+
+            # np.abs(np.cross(v, crystal_atoms_that_are_not_part_of_the_substructure - v[0]) / np.linalg.norm(v)) 
+            # then subtract the vanderwaals radius of the crystal atoms.
+        
+            print(np.unique(eval_, return_counts=True))
             if not np.all(eval_):
                 nhits-=1
                 rm.append(i)
@@ -133,38 +199,40 @@ for h in hits:
         rm.reverse()
         [local_hits.pop(i) for i in rm]
         # if there are any matches left, write to output.
-        if(len(local_hits)>0):
-            # spit out the matched atoms as a separate molecule file??
-            # outfile = join(outdir, '{0:s}_hits.mol2'.format(h.identifier))
-            # molwriter = ccdc.io.MoleculeWriter(outfile)
-            # mols = local_hits.superimpose()
-            # [molwriter.write(mm) for mm in mols]
-            # molwriter.close()
-            # write_c2m_file breaks with error: AttributeError: 'NoneType' object has no attribute 'substructure_index'
-            try:
-                local_hits.write_c2m_file(join(outdir, '{0:s}_hits.c2m'.format(h.identifier)))
-            except (AttributeError, NotImplementedError) as e:
-                print('{0:s} yielded attribute error when writing c2m file.'.format(h.identifier))
-            # count as a successful find
-            success_count += 1
-            a, d = zip(*[(s.constraints['ANGLE'], s.constraints['DIST']) for s in local_hits])
-
-            AVG_ANGLE = np.absolute(np.array(a)).mean()
-            STD_ANGLE = np.absolute(np.array(a)).std()
-            AVG_DIST = np.array(d).mean()
-            STD_DIST = np.array(d).std()
-
-            writer = ccdc.io.CrystalWriter(join(outdir, '{0:s}.cif'.format(h.identifier)), append=False)
-            writer.write(h)
-            writer.close()
-            vol = cm3 / h.cell_volume * 1000 # how many unit cells fit into a cubic centimetre
-            molar_dens = nhits * vol / avo # in mol/cm^3
-            
-            cwriter.writerow([h.identifier, h.cell_volume, vol/avo, nhits, molar_dens, AVG_ANGLE, STD_ANGLE, AVG_DIST, STD_DIST])
+    if(nhits>0):
+        # spit out the matched atoms as a separate molecule file??
+        # outfile = join(outdir, '{0:s}_hits.mol2'.format(h.identifier))
+        # molwriter = ccdc.io.MoleculeWriter(outfile)
+        # mols = local_hits.superimpose()
+        # [molwriter.write(mm) for mm in mols]
+        # molwriter.close()
+        # write_c2m_file breaks with error: AttributeError: 'NoneType' object has no attribute 'substructure_index'
+        try:
+            local_hits.write_c2m_file(join(outdir, '{0:s}_hits.c2m'.format(h.identifier)))
+        except (AttributeError, NotImplementedError) as e:
+            print('{0:s} yielded attribute error when writing c2m file.'.format(h.identifier))
+        # count as a successful find
+        success_count += 1
+        a, d = zip(*[(s.constraints['ANGLE'], s.constraints['DIST']) for s in local_hits])
+    
+        AVG_ANGLE = np.absolute(np.array(a)).mean()
+        STD_ANGLE = np.absolute(np.array(a)).std()
+        AVG_DIST = np.array(d).mean()
+        STD_DIST = np.array(d).std()
+    
+        vol = cm3 / h.cell_volume * 1000 # how many unit cells fit into a cubic centimetre
+        molar_dens = nhits * vol / avo # in mol/cm^3
+        
+        cwriter.writerow([h.identifier, h.cell_volume, vol/avo, nhits, molar_dens, AVG_ANGLE, STD_ANGLE, AVG_DIST, STD_DIST])
     total_count += 1
     print('{0:.2f}% complete ({1:d} of {2:d})'.format(total_count/tot*100., total_count, tot))
-end_time = time.time()
-elapsed = end_time - start_time
+    #writer = ccdc.io.CrystalWriter(join(outdir, '{0:s}.cif'.format(h.identifier)), append=False)
+    #writer.write(h)
+    #writer.close()
 f.close()
-print ("Total found = {0:d}, Total MOFs = {1:d}, success rate = {2:.3f}".format(success_count, total_count, float(success_count)/float(total_count)))
+end_time = time.time()
+elapsed = end_time-start_time
+#print('{0:d} structures found in {1:.2f} seconds.'.format(len(hits), elapsed))
+rate = float(success_count)/float(total_count) if total_count > 0 else 0.0
+print ("Total found = {0:d}, Total MOFs = {1:d}, success rate = {2:.3f}".format(success_count, total_count, rate))
 
